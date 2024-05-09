5f7affbc31076a252077927c21ca5f06e557db2f
Jessem Ettaghouti <jessemtaghouti.prepa@gmail.com>
2024-05-10 01:16:22 +0200

    Reglage base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue"
 import { fileURLToPath, URL } from "url"
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/portfolio-jessem/",
   plugins: [vue()],
   resolve: {
     alias: [
